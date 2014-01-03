24facb275fdb5f3069441380ebc14fecda0c276d
KK <keimar.kuhn+git@gmail.com>
2014-01-04 03:43:39 +0900

    Finish sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  include SessionsHelper
+
+  # CSRF
+  def handle_unverified_request
+    sign_out
+    super
+  end
 end
